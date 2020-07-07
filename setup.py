d15bb2b272b3bcd08c52c16c7d81cb29806095ff
RobDBennett <robbennett.work@gmail.com>
2020-07-07 12:37:48 -0700

    UGH!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ REQUIRED = [
     "numpy",
     "pandas",
     "sklearn",
-    "matplotlib",
+    "matplotlib.pyplot",
     "category_encoders"
 ]
 
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 setuptools.setup(
     name="lambdata_robdbennett",
-    version="0.0.5",
+    version="0.0.6",
     author="RobDBennett",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
